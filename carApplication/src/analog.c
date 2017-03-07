b1d4117ece42f2bfd5ead5188c8486ef638d0535
Robin Jonsson <jonrob-3@student.ltu.se>
2017-03-07 12:10:14 +0100

    Fixed several issues with ADC

--- carApplication/src/analog.c
+++ carApplication/src/analog.c
@@ -12,16 +12,20 @@ void initADC(void)
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;	//Enables clock for GPIOC
 	ADC->CCR |= ADC_CCR_ADCPRE;		//0b11;  //ADC clock 12.5MHz (100MHz prescaler 8)
 	GPIOC->MODER |= GPIO_MODER_MODER4;		//Sets GPIO PC4 to analog in
-	ADC->CCR |= ADC_SMPR1_SMP14;			//Sets sample rate for channel 14 to (111 = 480 cycles)
-	ADC->CCR |= ADC_CR1_EOCIE;			//Enables interrupts for EOC
+	ADC1->SMPR1 |= ADC_SMPR1_SMP14;			//Sets sample rate for channel 14 to (111 = 480 cycles)
+	ADC1->CR1 |= ADC_CR1_EOCIE;			//Enables interrupts for EOC
+	ADC1->CR2 |= ADC_CR2_ADON;  		//sets ADON bit (turns on ADC)
+	ADC1->CR2 |= ADC_CR2_SWSTART;		//Enables measuring on regular channels
+	ADC1->CR2 |= ADC_CR2_CONT;			//Enables continuous mode
 
-	ADC->CCR |= 0x00000001;  		//sets ADON bit (turns on ADC)
-	ADC->CCR |= ADC_CR2_SWSTART;		//Enables continuous mode
+	NVIC_EnableIRQ(ADC_IRQn);			//Enables interrupt handler
+	NVIC_SetPriority(ADC_IRQn, 18);
 }
 
 void ADC_IRQHandler (void) {
-	if(ADC_SR_EOC) {
-		ADCval = ADC_DR_DATA;		//Reads data from register to variable
-				//breakpoint prints out ADCval
+	if(ADC1->SR & ADC_SR_EOC) {
+		ADCval = ADC1->DR;		//Reads data from register to variable
+
 	}
+	//ADC1->SR &= ~ADC_SR_EOC;
 }
